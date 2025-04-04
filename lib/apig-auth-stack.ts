552516ca6096f6ba14c35640ccf9031a22b182d2
Marc Mendoza <777marc@gmail.com>
2025-04-04 12:32:45 -0400

    init

--- lib/apig-auth-stack.ts
+++ lib/apig-auth-stack.ts
@@ -1,16 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class ApigAuthStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApigAuthQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // create DynamoDB table for to-do items
+    const todoTable = new dynamodb.Table(this, "TodoTable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
