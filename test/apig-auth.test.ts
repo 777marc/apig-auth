d99496763014519f76b4f071a62ea5776a19b63a
Marc Mendoza <777marc@gmail.com>
2025-04-04 11:36:09 -0400

    Initial commit

---
+++ test/apig-auth.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApigAuth from '../lib/apig-auth-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/apig-auth-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApigAuth.ApigAuthStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
